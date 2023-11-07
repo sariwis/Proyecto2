7d95afab5d05fc3986e9257b203793c29101424c
Pedro2409 <pa.cedeno@uniandes.edu.co>
2023-11-06 21:09:00 -0500

    Update Conexion.py

--- Conexion.py
+++ Conexion.py
@@ -1,11 +1,26 @@
 #Carga de BD
 import psycopg2
 engine = psycopg2.connect(
-dbname="postgres",
+dbname="p2",
 user="postgres",
 password="proyecto2",
 host="proyecto2.c9pexl84mjtw.us-east-1.rds.amazonaws.com",
 port="5432"
 )
 
-"Se te actualiza este mensaje?"
+cursor = engine.cursor()
+
+query = """
+SELECT * 
+FROM pg_catalog.pg_tables 
+WHERE schemaname='public';"""
+cursor.execute(query)
+result = cursor.fetchall()
+result
+
+query = """
+SELECT * 
+FROM grado;"""
+cursor.execute(query)
+result = cursor.fetchall()
+result
